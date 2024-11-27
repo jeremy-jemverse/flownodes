c716b615a4ce18fff2aeae9ea2f6b1583418f687
Jeremy Snyman <jeremysnyman@LNMAC0034.broadband>
2024-11-27 23:33:59 +0000

    Include dist directory and add postinstall script for deployment

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 4000;
+app.use(express_1.default.json());
+// Sample API route
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from Node API!' });
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
