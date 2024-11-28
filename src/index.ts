fcb2ed8afa4072e35061f04c5b6ea70c9c1cf211
Jeremy Snyman <jeremysnyman@LNMAC0034.broadband>
2024-11-28 19:06:20 +0000

    fix: Update dotenv import and add dependency

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import httpRequestRoutes from './Nodes/HttpRequest/routes';
